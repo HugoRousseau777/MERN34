ab7bfd6c245e26fc02fe2b7089eea53d145b94ea
hugo <hugo0703@wanadoo.fr>
2023-02-13 11:14:17 +0100

    Header and Naviguation done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Header from './components/Header'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
   <Router>
     <div className='container'>
+      <Header/>
   <Routes>
     <Route path ='/' element={<Dashboard />}/>
     <Route path ='/login' element={<Login />}/>
